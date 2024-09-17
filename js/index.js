d02b405c35c0a633fa36cdcf6e92484f353a39f5
Griffin Leventhal <120704526+GriffinLev039@users.noreply.github.com>
2024-09-17 16:29:24 -0400

    initial commit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const sigPohot = document.getElementById("significantIMG");
+const imgContainer = document.getElementById("StickImage");
+
+document.addEventListener('scroll',()=>{
+    console.log(imgContainer.scrollTop);
+})
